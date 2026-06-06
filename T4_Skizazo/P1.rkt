cabf068cdbc7928eb24c1b5d5f09501c215a7787
Big-Diker <jugailoloye@gmail.com>
2026-06-05 21:53:53 -0400

    P1 comentada, P2 hecha y comentada

--- T4_Skizazo/P1.rkt
+++ T4_Skizazo/P1.rkt
@@ -1,11 +1,30 @@
 #lang scheme
+;; icebreaker-simple
+;; Aplica una funcion de descifrado (daemon) a una lista.
+;; Parametros: 
+;;   -daemon: funcion matemática a definir.
+;;   -puertos: lista de numeros a procesar.
+;; Retorno: Nueva lista con el daemon aplicado. Mantiene su orden original 
+(define (icebreaker-simple daemon puertos)
+  (cond
+    ((null? puertos) '())
+    ((number? (car puertos))
+     (append (list (daemon (car puertos))) (icebreaker-simple daemon (cdr puertos))))))
 
-(define alfa (lambda (x) (modulo (+ x 15) 10)))
-
+;; icebreaker-cola
+;; Hace lo mismo que icebreaker-simple, pero implementado con recursion de cola, 
+;; es decir, usa menos memoria y es mas eficiente.
+;; Parametros:
+;;   -daemon: funcion matemática a definir.
+;;   -puertos: lista de numeros a procesar.
+;;    La funcion de recursividad interna "rec" tiene ademas, los siguientes parametros:
+;;       +resto: copia de la lista puertos, el cual va decreciendo en tamaño por cada recursion
+;;       +acum: lista en la que se van guardando los numeros procesados por cada recursion
+;; Retorno: Nueva lista con el daemon aplicado. Mantiene su orden original
 (define (icebreaker-cola daemon puertos)
-    (cond
-        ((null? puertos) '())
-        ((number? (car puertos))
-            (append (list (daemon (car puertos))) (icebreaker-cola daemon (cdr puertos))))))
-
-(displayln (icebreaker-cola (lambda (x) (* x 2)) '()))
+  (let rec ((resto puertos) (acum '()))
+  (cond
+    ((null? resto)
+     (reverse acum))
+    ((number? (car resto))
+     (rec (cdr resto) (cons (daemon (car resto)) acum))))))
--- T4_Skizazo/P2.rkt
+++ T4_Skizazo/P2.rkt
@@ -1,1 +1,22 @@
 #lang scheme
+
+;; crear-gusano
+;; Fabrica y retorna un "gusano" (una funcion lambda) capaz de recorrer un arbol N-ario.
+;; Reemplaza con X los nodos que no cumplan la condicion dada (vulnerabilidad), manteniendo intactos los vulnerables y la estructura.
+;; Parametros:
+;;   -vulnerabilidad: funcion (predicado) que evalua un numero y retorna #t o #f.
+;;    La funcion interna "gusano" devuelta tiene ademas el siguiente parametro:
+;;       +red-neuronal: lista N-aria (puede contener sublistas) a analizar y procesar.
+;; Retorno: Una funcion lambda que, al ejecutarse sobre una red neuronal, devuelve la lista con las marcas.
+(define (crear-gusano vulnerabilidad)
+  (letrec ((gusano
+            (lambda (red-neuronal)
+              (cond
+                ((null? red-neuronal) '())
+                ((number? (car red-neuronal)) 
+                 (if (vulnerabilidad (car red-neuronal))    
+                     (cons (car red-neuronal) (gusano (cdr red-neuronal)))
+                     (cons 'X (gusano (cdr red-neuronal)))))
+                ((pair? (car red-neuronal))
+                 (cons (gusano (car red-neuronal)) (gusano (cdr red-neuronal))))))))
+    gusano))
